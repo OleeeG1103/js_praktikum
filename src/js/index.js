e509403173d2a94e11cc62e07be023eba77eed34
Oleg <bukreevoleg3001@gmail.com>
2021-08-18 22:52:09 +0300

    add popup add-card, add animation popup, add array initial card,

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,94 @@
-// open/close edit popup
+// const show/hide edit popup
+const profileEditBtn = document.querySelector('.profile-info--editBtn');
+const popupEditProfile = document.querySelector('#popupEditProfile');
+const closePopupEditProfile = document.querySelector('.popup-close--editProfile');
 
-let profileEditBtn = document.querySelector('.profile-info--editBtn');
-let profileAddBtn = document.querySelector('.profile-add-btn');
-let closePopup = document.querySelector('.popup-close');
-let popup = document.querySelector('.popup-wrapper');
+// const form edit popup
+const profileName = document.querySelector('#profile-name');
+const profilePosition = document.querySelector('#profile-position');
 
-function popupEditShow() {
+const popupFormEditProfile = document.querySelector('.popup-form--edit-profile');
+const popupFormName = popupFormEditProfile.querySelector('.popup-form--name');
+const popupFormPosition = popupFormEditProfile.querySelector('.popup-form--position');
+const popupFormEditProfileBtn = popupFormEditProfile.querySelector('.popup-form--btn');
+
+// const show/hide add card popup
+const profileAddCardBtn = document.querySelector('.profile-add-card--btn');
+const popupAddCard = document.querySelector('#popupAddCard');
+const closePopupAddCard = document.querySelector('.popup-close--addCard');
+
+// const form add card popup
+
+const popupFormAddCard = document.querySelector('.popup-form--add-card');
+const popupFormNameCard = popupFormAddCard.querySelector('.popup-form--name');
+const popupFormLinkImg = popupFormAddCard.querySelector('.popup-form--link');
+const popupFormAddCardBtn = popupFormAddCard.querySelector('.popup-form--btn');
+
+// show/hide popup
+const popupShow = (popup) => {
 	popup.classList.add('popup-wrapper--show');
-}
+};
 
-profileEditBtn.addEventListener('click', popupEditShow);
-
-function popupEditClose() {
+const popupHide = (popup) => {
 	popup.classList.remove('popup-wrapper--show');
-}
+};
 
-closePopup.addEventListener('click', popupEditClose);
-
-document.addEventListener('click', (e) => {
-	if(e.target === popup) {
-		popupEditClose();
-	}
+// forms popup
+profileEditBtn.addEventListener('click', () => {
+	popupShow(popupEditProfile);
+	popupFormName.value = profileName.textContent;
+    popupFormPosition.value = profilePosition.textContent;
 });
+
+profileAddCardBtn.addEventListener('click', () => {
+	popupShow(popupAddCard);
+	popupFormNameCard.placeholder = 'Название';
+    popupFormLinkImg.placeholder = 'Ссылка на картинку';
+});
+
+closePopupEditProfile.addEventListener('click', () => {
+	popupHide(popupEditProfile);
+});
+
+closePopupAddCard.addEventListener('click', () => {
+	popupHide(popupAddCard);
+});
+
+
+//function popupEditShow() {
+//	popupFormName.value = profileName.textContent;
+//    popupFormPosition.value = profilePosition.textContent;
+//    
+//	popup.classList.add('popup-wrapper--show');
+//}
+
+//profileEditBtn.addEventListener('click', popupEditShow);
+
+
+//function popupEditClose() {
+//	popup.classList.remove('popup-wrapper--show');
+//}
+
+//closePopup.addEventListener('click', popupEditClose);
+
+//document.addEventListener('click', (e) => {
+//	if(e.target === popup) {
+//		popupEditClose();
+//	}
+//});
 
 
 // form edit popup 
 
-let profileName = document.querySelector('#profile-name');
-let popupFormName = document.querySelector('.popup-form--name');
-let profilePosition = document.querySelector('#profile-position');
-let popupFormPosition = document.querySelector('.popup-form--position');
-let popupFormBtn = document.querySelector('.popup-form--btn');
-let popupEditForm = document.querySelector('.popup-form');
 
-popupFormName.value = profileName.textContent;
-popupFormPosition.value = profilePosition.textContent;
 
 function editProfile(evt) {
 	evt.preventDefault();
 	profileName.textContent = popupFormName.value;
 	profilePosition.textContent = popupFormPosition.value;
-	popupEditClose();
+	//popupEditClose();
+	//popupHide();
+	popupHide(popupEditProfile);
 }
 
-popupEditForm.addEventListener('submit', editProfile);
+popupFormEditProfile.addEventListener('submit', editProfile);
